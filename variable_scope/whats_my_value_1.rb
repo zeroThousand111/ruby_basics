7ede748297f24439d1027c7f349a83d09239d451
zeroThousand111 <zeroThousand111@proton.me>
2023-02-12 15:13:51 +0000

    update whats_my_value_1.rb to include how to output the argument passed to my_value method

--- variable_scope/whats_my_value_1.rb
+++ variable_scope/whats_my_value_1.rb
@@ -2,7 +2,8 @@ a = 7
 
 def my_value(b)
   b += 10
+  # puts b - this line here would enable the script to output the value of b, the argument passed to my_value
 end
 
-my_value(a)
+my_value(a) # there is no instruction to output the argument a in the method my_value
 puts a
