7b43aed4c0c67dfc6890f9121b6a088ec4b07f1a
zeroThousand111 <zeroThousand111@proton.me>
2023-03-08 16:43:24 +0000

    code now has simpler loop/do/end structure

--- loops1/control_the_loop.rb
+++ loops1/control_the_loop.rb
@@ -1,7 +1,10 @@
-iterations = 5
-i = 1
+iterations = 1
+i = 0
 
-until i == (iterations + 1) do
-    puts "Number of iterations = #{i}"
-    i = i + 1
+loop do
+	i = i + 1
+  	puts "Number of iterations = #{i}"
+	if i == 5 
+    break
+  end
 end
