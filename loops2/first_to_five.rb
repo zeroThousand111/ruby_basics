7e5c9509a23f6f7474f365d2054e02a05fc16f94
Graham Jarvis <zeroThousand111@proton.me>
2023-03-11 12:38:29 +0000

    refactor to include a next instead of an if/else loop

--- loops2/first_to_five.rb
+++ loops2/first_to_five.rb
@@ -5,8 +5,8 @@ loop do
   number_a += rand(2)
   number_b += rand(2)
   puts "A: #{number_a} | B: #{number_b}"
-  if number_a == 5 || number_b == 5
-      puts "Five was reached!"
-      break
-  end
+  next unless number_a == 5 || number_b == 5
+
+  puts "Five was reached!"
+  break
 end
