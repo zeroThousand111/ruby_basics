61ee9a8d7a75a9bcbcd2a1b655c6ed79a784a269
Graham Jarvis <zeroThousand111@proton.me>
2023-03-11 15:38:21 +0000

    create repeat_after_me.rb to answer exercise of the same name

---
+++ user_input/repeat_after_me.rb
@@ -0,0 +1,10 @@
+# Example output:
+
+# >> Type anything you want:
+# This is what I typed.
+# This is what I typed.
+
+puts "> > Type anything you want:"
+user_input = gets.chomp
+
+puts user_input
