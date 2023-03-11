d886d547c9df66fbd78522795068773ec934f5e0
Graham Jarvis <zeroThousand111@proton.me>
2023-03-11 19:07:03 +0000

    create print_something_2.rb to answer exercise of the same name

---
+++ user_input/print_something_2.rb
@@ -0,0 +1,20 @@
+# create a loop that encompasses whole code
+
+loop do
+
+    # get user input, remove carriage return and turn it to lowercase
+    puts ">> Do you want me to print 'something'? (y/n)"
+    user_input = gets.chomp
+
+    # conditional response
+
+    if user_input == "y"
+        puts "'something'"
+        break
+    elsif user_input == "n"
+        break
+    else
+        puts "Invalid input!  Please press y or n"
+    end
+
+end
