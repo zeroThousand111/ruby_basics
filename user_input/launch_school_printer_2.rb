6b98f1bdf9331012508a652505cbde5eb1503dec
Graham Jarvis <zeroThousand111@proton.me>
2023-03-12 20:21:26 +0000

    create launch_school_printer_2.rb to answer exercise of the same name

---
+++ user_input/launch_school_printer_2.rb
@@ -0,0 +1,17 @@
+number_of_lines = nil
+
+loop do
+  puts '>> How many output lines do you want? Enter a number >= 3 (q to quit):'
+  number_of_lines = gets.chomp.downcase
+  break if number_of_lines == "q"
+  number_of_lines = number_of_lines.to_i
+  break if number_of_lines >= 3
+  puts ">> That's not enough lines."
+end
+
+number_of_lines = number_of_lines.to_i
+
+while number_of_lines > 0
+  puts 'Launch School is the best!'
+  number_of_lines -= 1
+end
