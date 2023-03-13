2373bbb000e1901988667ccd4a3bc8d829158d66
Graham Jarvis <zeroThousand111@proton.me>
2023-03-13 11:10:05 +0000

    add extra loop to make the program repeat until user quits

--- user_input/launch_school_printer_2.rb
+++ user_input/launch_school_printer_2.rb
@@ -1,17 +1,19 @@
-number_of_lines = nil
-
 loop do
-  puts '>> How many output lines do you want? Enter a number >= 3 (q to quit):'
-  number_of_lines = gets.chomp.downcase
+  number_of_lines = nil
+  
+  loop do
+    puts '>> How many output lines do you want? Enter a number >= 3 (q to quit):'
+    number_of_lines = gets.chomp.downcase
+    break if number_of_lines == "q"
+    number_of_lines = number_of_lines.to_i
+    break if number_of_lines >= 3
+    puts ">> That's not enough lines."
+  end
+  
   break if number_of_lines == "q"
-  number_of_lines = number_of_lines.to_i
-  break if number_of_lines >= 3
-  puts ">> That's not enough lines."
-end
-
-number_of_lines = number_of_lines.to_i
-
-while number_of_lines > 0
-  puts 'Launch School is the best!'
-  number_of_lines -= 1
+  
+  while number_of_lines > 0
+    puts 'Launch School is the best!'
+    number_of_lines -= 1
+  end
 end
