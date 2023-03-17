99dbf3166d7f751d35eebe2421ab22c299ede3ed
Graham Jarvis <zeroThousand111@proton.me>
2023-03-17 08:17:17 +0000

    added more tests of percent strings

--- strings/quote_confusion.rb
+++ strings/quote_confusion.rb
@@ -1,1 +1,11 @@
-puts "It's now 12 o'clock."
+puts 'It\'s now 12 o\'clock.'   # single quotes
+puts "It's now 12 o'clock."     # double-quotes
+
+#Percent Strings
+puts %q(It's now 12 o'clock.)   # single-quote percent string (but doesn't need escape character)
+puts %Q(It's now 12 o'clock.)   # double-quote percent string
+
+# String Interpolation Test
+n = 12
+puts %q(It's now #{n} o'clock.) # can't use string interpolation
+puts %Q(It's now #{n} o'clock.) # can use string interpolation
