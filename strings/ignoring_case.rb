7fff6b499f99737810f6a45898e243581aa9c177
Graham Jarvis <zeroThousand111@proton.me>
2023-03-17 15:26:56 +0000

    create ignorning_case.rb to answer the exercise of the same name

---
+++ strings/ignoring_case.rb
@@ -0,0 +1,13 @@
+name = 'Roger'
+
+if name.downcase == 'RoGeR'.downcase 
+    puts "true" 
+else 
+    puts "false"
+end
+
+if name.downcase == 'DAVE'.downcase 
+    puts "true" 
+else 
+    puts "false"
+end
