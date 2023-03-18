1c94322bc346d63a713dcd9ed9ff2890f95ae54e
Graham Jarvis <zeroThousand111@proton.me>
2023-03-18 09:03:43 +0000

    create one_isnt_enough.rb to answer exercise of the same name

---
+++ arrays/one_isnt_enough.rb
@@ -0,0 +1,8 @@
+# can't touch this code
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
+
+# my code
+my_pets[1] = pets[1]
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
