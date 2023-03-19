2f85eefed8969576d3835bfa3e87b3a1aa7b76eb
Graham Jarvis <zeroThousand111@proton.me>
2023-03-19 16:26:05 +0000

    create which_collection.rb to answer the exercise of the same name

---
+++ hashes/which_collection.rb
@@ -0,0 +1,14 @@
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+
+car_array = [   
+    ['type', 'sedan'],
+    ['color', 'blue'],
+    ['year', 2003]
+    ]
+    
+p car
+p car_array
