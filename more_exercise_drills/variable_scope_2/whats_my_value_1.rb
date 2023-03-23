670e4b8ee8c8baa305a1dd73d88cd8e1a484ee58
Graham Jarvis <zeroThousand111@proton.me>
2023-03-23 09:41:07 +0000

    create object_id outputs in the code to determine what is happening to a and b

--- more_exercise_drills/variable_scope_2/whats_my_value_1.rb
+++ more_exercise_drills/variable_scope_2/whats_my_value_1.rb
@@ -1,8 +1,12 @@
 a = 7
 
+puts "Start: The value of a is #{a} and the id of a is #{a.object_id}."
+
 def my_value(b)
+  puts "Start method: The value of b is #{b} and the id of b is #{b.object_id}."
   b += 10
+  puts "End method: The value of b is now  #{b} and the id of b is #{b.object_id}."
 end
 
 my_value(a)
-puts a
+puts "End: The value of a is still #{a} and the id of a is #{a.object_id}."
